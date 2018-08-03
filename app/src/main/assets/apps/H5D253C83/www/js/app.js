cde64f8d77bf7581f6162c09ad42e60a0c32c78f
guomin <yushi5344@gmail.com>
2018-08-03 16:38:38 +0800

    AC单领料出库--按缸

--- app/src/main/assets/apps/H5D253C83/www/js/app.js
+++ app/src/main/assets/apps/H5D253C83/www/js/app.js
@@ -142,6 +142,38 @@ var checkSaveByJuan=function(submitinfo,callback){
 	mui.toast(data.msg);
     location.reload();
 }
+//卷验按缸入库提交
+var checkSaveByGang=function(submitinfo,callback){
+	if (submitinfo.rukuDate.length =='') {
+		return callback('请选择入库日期');
+	}
+	if (submitinfo.kuweiId.length =='') {
+		return callback('请选择仓库');
+	}
+	if (submitinfo.kuquId.length =='') {
+		return callback('请选择库位');
+	}
+	if (submitinfo.checkId.length =='') {
+		return callback('请扫描布卷');
+	}
+	var state = getState();
+    var creater=state.account;
+	var arr={
+		'rukuDate':submitinfo.rukuDate,
+		'kuweiId':submitinfo.kuweiId,
+		'kuquId':submitinfo.kuquId,
+		'checkId':submitinfo.checkId,
+		'memo':submitinfo.memo,
+        'creater':creater,
+		'token':config.token
+	};
+	var data=request('POST',arr,config.apimethod.checkSaveByGang);
+	if(!data.success){
+		return false;
+	}
+	mui.toast(data.msg);
+    location.reload();
+}
 //BD单领用登记列表
 var bdListAdd=function(page,searchKey){
 	var arr={
@@ -165,6 +197,23 @@ var getMlDataChu=function(codeId){
 	}
 	return data.params;
 }
+/**
+ * 通过卷号获取出库整缸布卷信息
+ * @param {Object} codeId
+ * @return {Object}
+ */
+var getMlDataChuByGang=function(codeId){
+	var arr={
+		codeId:codeId,
+		token:config.token
+	};
+	var data=request('POST',arr,config.apimethod.getMlDataChuByGang);
+	if(!data.success){
+		return;
+	}
+	return data.params;
+}
+
 var BDChukuSaveByJuan=function(submitInfo,callback){
 	console.log(JSON.stringify(submitInfo));
 	if (submitInfo.chukuDate.length =='') {
@@ -222,6 +271,11 @@ var suppierRc=function(){
 	var data=request('POST',arr,config.apimethod.suppierRc);
 	return data.params;
 }
+/**
+ * AC单按卷出库
+ * @param {Object} submitInfo
+ * @param {Object} callback
+ */
 var ACChukuSaveByJuan=function(submitInfo,callback){
 	console.log(JSON.stringify(submitInfo));
 	if (submitInfo.chukuDate.length =='') {
@@ -253,6 +307,37 @@ var ACChukuSaveByJuan=function(submitInfo,callback){
 	mui.toast(data.msg);
 	mui.back();
 }
+var ACChukuSaveByGang=function(submitInfo,callback){
+	console.log(JSON.stringify(submitInfo));
+	if (submitInfo.chukuDate.length =='') {
+		return callback('请选择出库日期');
+	}
+	if (submitInfo.kuweiId.length =='') {
+		return callback('请选择仓库');
+	}
+	if (submitInfo.madanId.length =='') {
+		return callback('请扫描布卷');
+	}
+	var state = getState();
+	var creater=state.account;
+	var arr={
+		'chukuDate':submitInfo.chukuDate,
+		'kuweiId':submitInfo.kuweiId,
+		'kuweiIdru':submitInfo.kuweiIdru,
+		'planId':submitInfo.planId,
+		'productId':submitInfo.productId,
+		'madanId':submitInfo.madanId,
+		'memo':submitInfo.memo,
+		'creater':creater,
+		'token':config.token
+	};
+	var data=request('POST',arr,config.apimethod.ACChukuSaveByGang);
+	if(!data.success){
+		return false;
+	}
+	mui.toast(data.msg);
+	mui.back();
+}
 //初始化扫码监听事件
 var initMe=function ()
 {
@@ -262,4 +347,150 @@ var initMe=function ()
 var getCode=function ()
 {
     return code=plus.Scaner.getCode("");
+}
+var getGangData=function(codeId){
+	var arr={'token':config.token,codeId:codeId};
+	var data=request('POST',arr,config.apimethod.getGangData);
+	if(!data.success){
+		return ;
+	}
+	return data.params;
+}
+/**
+ * li标签左滑删除
+ */
+var listenDelete=function(){
+	mui(".mui-table-view").on('tap','.mui-btn-red',function(){
+    	var btnArray = ['是', '否'];
+        var li = this.parentNode.parentNode;
+        mui.confirm("确定删除?", "Message", btnArray, function (e) {
+            if (e.index == 0) {
+                li.parentNode.removeChild(li);
+            }else {
+                mui.swipeoutClose(li);
+            }
+        });
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var as = 'pop-in'; // 默认窗口动画
+var _openw = null;
+// 预创建二级页面
+var preate = {};
+//打开新页面
+function clicked(id, param, a, s) {
+    var obj = {
+        preate: true
+    };
+    if(_openw) {
+        return;
+    }
+    a || (a = as);
+    _openw = preate[id];
+    if(_openw) {
+        _openw.showded = true;
+        _openw.show(a, null, function() {
+        _openw = null; //避免快速点击打开多个页面
+        });
+    } else {
+    var wa = plus.nativeUI.showWaiting();
+    obj = mui.extend(obj, param);
+    _openw = plus.webview.create(id, id, {
+        scrollIndicator: 'none',
+        scalable: false,
+        popGesture: 'hide'
+        }, obj);
+        preate[id] = _openw;
+        _openw.setStyle({
+        'popGesture': 'none'
+        });
+        _openw.addEventListener('loaded', function() { //页面加载完成后才显示
+            setTimeout(function() { //延后显示可避免低端机上动画时白屏
+            wa.close();
+            _openw.showded = true;
+            s || _openw.show(a, null, function() {
+            _openw = null; //避免快速点击打开多个页面
+            });
+            s && (_openw = null); //避免s模式下变量无法重置
+        }, 10);
+        }, false);
+        _openw.addEventListener('hide', function() {
+        _openw && (_openw.showded = true);
+        _openw = null;
+        }, false);
+        _openw.addEventListener('close', function() { //页面关闭后可再次打开
+        _openw = null;
+        preate[id] && (preate[id] = null); //兼容窗口的关闭
+        }, false);
+    }
 }
